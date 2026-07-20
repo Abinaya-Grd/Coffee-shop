da7f5ba9a7266c8eab545a384beab99fae6cd906
Abinaya M <abinayamanokaran2006@gmail.com>
2026-07-20 22:08:41 +0530

    Improve responsive design and add mobile navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,27 @@
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import {
   FaCoffee,
   FaSearch,
   FaUser,
   FaCartArrowDown,
+  FaBars,
+  FaTimes,
 } from "react-icons/fa";
 
 function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className="navbar">
 
-      
+      {/* Logo */}
       <div className="logo">
         <FaCoffee className="logo-icon" />
 
@@ -21,59 +31,70 @@ function Navbar() {
         </div>
       </div>
 
-      
-      <nav>
+      {/* Navigation */}
+      <nav className={menuOpen ? "nav active" : "nav"}>
+
         <ul className="nav-links">
 
           <li>
-            <NavLink to="/" end>
+            <NavLink to="/" end onClick={closeMenu}>
               Home
             </NavLink>
           </li>
 
           <li>
-            <NavLink to="/about">
+            <NavLink to="/about" onClick={closeMenu}>
               About
             </NavLink>
           </li>
 
           <li>
-            <NavLink to="/menu">
+            <NavLink to="/menu" onClick={closeMenu}>
               Menu
             </NavLink>
           </li>
 
           <li>
-            <NavLink to="/gallery">
+            <NavLink to="/gallery" onClick={closeMenu}>
               Gallery
             </NavLink>
           </li>
 
-         <li>
-          <NavLink to="/review">
-          Reviews
-         </NavLink>
-          </li>   
           <li>
-            <NavLink to="/blog">
+            <NavLink to="/review" onClick={closeMenu}>
+              Reviews
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/blog" onClick={closeMenu}>
               Blog
             </NavLink>
           </li>
 
           <li>
-            <NavLink to="/contact">
+            <NavLink to="/contact" onClick={closeMenu}>
               Contact
             </NavLink>
           </li>
 
         </ul>
+
       </nav>
 
-     
+      {/* Icons */}
       <div className="nav-icons">
         <FaSearch />
         <FaUser />
         <FaCartArrowDown />
+      </div>
+
+      {/* Hamburger Menu */}
+      <div
+        className="menu-toggle"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
 
     </header>
